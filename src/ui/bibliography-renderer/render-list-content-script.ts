8d480fc5f95c37df31c016b163e2cd09b642b281
AlekseiTepljakov <alex@starspirals.net>
2021-09-14 16:45:11 +0300

    Added some token parsing due to [[toc]] plugin
    Results are still the same, looks like an issue with the
    overall architecture. Need to investigate further.

--- src/ui/bibliography-renderer/render-list-content-script.ts
+++ src/ui/bibliography-renderer/render-list-content-script.ts
@@ -1,9 +1,101 @@
-import { Reference } from "../../model/reference.model";
+import {Reference} from "../../model/reference.model";
+
 
 export default function (context) {
     return {
         plugin: function (markdownIt, _options) {
             const contentScriptId = context.contentScriptId;
+            var gstate;
+
+            // Let's try defining the [references] tag
+            // Used markdownit-table-of-contents plugin as reference here
+            function references_list(state, silent) {
+
+                // Marker pattern
+                var markerPattern: RegExp = /^\[references\]/im;
+                var match;
+                var token;
+
+                if (state.src.charCodeAt(state.pos) !== 0x5B) {
+                    return false;
+                }
+
+                if (silent){
+                    return false;
+                }
+
+                match = markerPattern.exec(state.src.substr(state.pos));
+                match = !match ? [] : match.filter(function (m) {
+                    return m;
+                });
+                if (match.length < 1) {
+                    return false;
+                }
+
+                token = state.push('references_open', 'references', 1);
+                token.markup = '[references]';
+                token = state.push('references_body', '', 0);
+                token = state.push('references_close', 'references', -1);
+
+                var newline = state.src.indexOf('\n', state.pos);
+                if (newline !== -1) {
+                    state.pos = newline;
+                } else {
+                    state.pos = state.pos + state.posMax + 1;
+                }
+
+                return true;
+
+            }
+
+            markdownIt.inline.ruler.after('emphasis', 'references_list', references_list);
+
+            markdownIt.renderer.rules.references_open = function (tokens, index) {
+                return "<div id='the_bibtex_references'>";
+            }
+
+            markdownIt.renderer.rules.references_body = function (tokens, index) {
+
+                // Can I locate the references_list token?
+                var IDs;
+                for (let kk=0; kk<gstate.tokens.length; kk++){
+                    if (gstate.tokens[kk].type == "reference_list"){
+                        IDs = gstate.tokens[kk]["attrs"][0][1];
+                    }
+                }
+
+                // Do the message parsing stuff now...........
+                if (IDs.length === 0) return "";
+
+                const script: string = `
+					webviewApi.postMessage("${contentScriptId}", ${JSON.stringify(
+                    IDs
+                )}).then(html => {
+						const referenceListView = document.getElementById("references_list");
+						const referenceTitleView = document.getElementById("references_title");
+
+						if (html !== "") referenceTitleView.style.display = "block";
+
+						referenceListView.innerHTML = html;
+					});
+					return true;
+				`;
+
+                return `
+					<h1 id="references_title" style="display:none">References</h1>
+					<div id="references_list">Loading ...</div>
+					<style onload='${script.replace(/\n/g, " ")}'></style>
+				`;
+
+            }
+
+            markdownIt.renderer.rules.references_close = function (tokens, index) {
+                return "</div>";
+            }
+
+            markdownIt.core.ruler.push('grab_state', function(state) {
+                gstate = state;
+            });
 
             /* Appends a new custom token for references list */
             markdownIt.core.ruler.push("reference_list", async (state) => {
@@ -47,34 +139,33 @@ export default function (context) {
             });
 
             /* Define how to render the previously defined token */
-            markdownIt.renderer.rules["reference_list"] = renderReferenceList;
-
-            function renderReferenceList(tokens, idx, options) {
-                // IDs are an array of such type ["Steward2019useagriculturalrobots"]
-                let IDs: string[] = tokens[idx]["attrs"][0][1];
-                if (IDs.length === 0) return "";
-
-                const script: string = `
-					webviewApi.postMessage("${contentScriptId}", ${JSON.stringify(
-                    IDs
-                )}).then(html => {
-						const referenceListView = document.getElementById("references_list");
-						const referenceTitleView = document.getElementById("references_title");
-
-						if (html !== "") referenceTitleView.style.display = "block";
-
-						referenceListView.innerHTML = html;
-					});
-					return false;
-				`;
-
-                return `
-                    <span>Hey, I am returning this anyway, but something is wrong!</span>
-					<h1 id="references_title" style="display:none">References</h1>
-					<div id="references_list"></div>
-					<style onload='${script.replace(/\n/g, " ")}'></style>
-				`;
-            }
+            // markdownIt.renderer.rules["reference_list"] = renderReferenceList;
+            //
+            // function renderReferenceList(tokens, idx, options) {
+            //     // IDs are an array of such type ["Steward2019useagriculturalrobots"]
+            //     let IDs: string[] = tokens[idx]["attrs"][0][1];
+            //     if (IDs.length === 0) return "";
+            //
+            //     const script: string = `
+			// 		webviewApi.postMessage("${contentScriptId}", ${JSON.stringify(
+            //         IDs
+            //     )}).then(html => {
+			// 			const referenceListView = document.getElementById("references_list");
+			// 			const referenceTitleView = document.getElementById("references_title");
+            //
+			// 			if (html !== "") referenceTitleView.style.display = "block";
+            //
+			// 			referenceListView.innerHTML = html;
+			// 		});
+			// 		return false;
+			// 	`;
+            //
+            //     return `
+			// 		<h1 id="references_title" style="display:none">References</h1>
+			// 		<div id="references_list">Loading ...</div>
+			// 		<style onload='${script.replace(/\n/g, " ")}'></style>
+			// 	`;
+            // }
         },
     };
 }
